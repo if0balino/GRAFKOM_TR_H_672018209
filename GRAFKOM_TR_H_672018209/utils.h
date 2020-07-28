2692f006042e82f9b5cac71a40150e630615ca76
Rietts Andreas Ruff <Riettsruff@gmail.com>
2020-07-28 14:22:17 +0700

    Add Import File Utils

--- GRAFKOM_TR_H_672018209/utils.h
+++ GRAFKOM_TR_H_672018209/utils.h
@@ -1,6 +1,25 @@
 #pragma once
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
 struct vec3 {
 	float x;
 	float y;
 	float z;
 };
+
+ vector<string> importFile(string fileName) {
+	vector <string> vertex;
+
+	ifstream file(fileName);
+	string str;
+	while (getline(file, str)) {
+		cout << str << endl;
+		vertex.push_back(str);
+	}
+
+	return vertex;
+}
